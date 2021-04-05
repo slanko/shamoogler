2eb39407748bf81797182cad29f39a69ef26d3b1
slink <QMBFXSL@gmail.com>
2021-04-06 03:23:18 +1200

    Initial Initial commit

---
+++ Assets/camLerper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camLerper : MonoBehaviour
+{
+    [SerializeField] Transform followTransform;
+    [SerializeField] float lerpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, followTransform.position, lerpSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/playerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    [SerializeField] float moveSpeed, turnSpeed;
+    [SerializeField] KeyCode upKey, downKey, leftKey, rightKey;
+    Animator anim;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(0, 0, v * moveSpeed * Time.deltaTime);
+        transform.Rotate(0, h * turnSpeed * Time.deltaTime, 0);
+        anim.SetFloat("walkey", v);
+    }
+}
